1d4db665b2ec2d60c955fd3306483bc9738ff4ec
Felix Nunez <felix.nunez@vass.com.mx>
2024-12-07 14:18:01 -0700

    Plantilla base

---
+++ src/main/java/edu/mx/utdelacosta/BackendJava02Application.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJava02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJava02Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mx/utdelacosta/BackendJava02ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJava02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
