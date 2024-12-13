95624ef5ea3fa6ac19733831c316ebb3d4da0fb8
Felix Nunez <felix.nunez@vass.com.mx>
2024-12-12 21:20:28 -0700

    Metodo 2 de consumo de servicios
    Busqueda por nombre

--- src/main/java/edu/mx/utdelacosta/controllers/LenguajesProgramacionController.java
+++ src/main/java/edu/mx/utdelacosta/controllers/LenguajesProgramacionController.java
@@ -1,13 +1,18 @@
 package edu.mx.utdelacosta.controllers;
 
+import edu.mx.utdelacosta.entity.LenguajesProgramacion;
 import edu.mx.utdelacosta.services.LenguajesProgramacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v2")
@@ -24,6 +29,20 @@ public class LenguajesProgramacionController {
         respuesta.put("head", "Lista de lenguajes");
         respuesta.put("body", lenguajesService.listaLenguajes());
         return respuesta;
+    }
+
+    @GetMapping("/lenguajes-programacion/{nombre}")
+    public Map<String, Object> getLenguajeProgramacionPorNombre(@PathVariable String nombre){
+        LenguajesProgramacion lenguaje = lenguajesService.busquedaPorNombre(nombre);
+
+        Map<String, Object> respuesta = new HashMap<>();
+        if(lenguaje == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Lenguaje no encontrado");
+        }else{
+            respuesta.put("head", "Lenguaje encontrado");
+            respuesta.put("body", lenguaje);
+            return respuesta;
+        }
     }
 
 }
